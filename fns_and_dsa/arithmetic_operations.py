10e2977014c30b021a11b7dcfa87893de8fdb0c3
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-06-08 08:00:42 -0800

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
