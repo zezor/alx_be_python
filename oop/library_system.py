a0f3f6a79273bc33e0da8eb4ba57f580f90e8093
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-06-22 13:02:22 -0800

    Create library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,40 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def get_details(self):
+        return f"Book: {self.title} by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def get_details(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def get_details(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only Book, EBook, or PrintBook instances can be added.")
+
+    def list_books(self):
+        for book in self.books:
+            print(book.get_details())
